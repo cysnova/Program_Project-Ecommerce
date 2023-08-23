a335609a8c35a217db2a3c57913ccd82c22ccc07
Nova Sin <cysnova@gmail.com>
2023-08-22 19:00:03 -0700

    done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,8 +3,7 @@ import { redirect } from "next/navigation";
 
 import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
-// import { ThemeToggle } from "@/components/theme-toggle";
-
+import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -14,7 +13,6 @@ const Navbar = async () => {
     redirect('/sign-in');
   }
 
-  
   const stores = await prismadb.store.findMany({
     where: {
       userId,
@@ -24,16 +22,15 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-    
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
-          {/* <ThemeToggle /> */}
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
   );
 };
-
+ 
 export default Navbar;
